b078543b0b037b7e43afeced37b83529b5c2a9d0
Carl Brandon Valentine <brandonvalentine23@gmail.com>
2025-01-14 00:36:01 +0800

    Account Edit Test

---
+++ tests/Integration/AccountEditTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AccountEdit extends TestCase
+{
+    /**
+     * Test account balance can be set and retrieved.
+     */
+    public function testAccountBalance()
+    {
+
+        // Assert: Check if the balance is 100
+        $this->assertEquals(100, 100);
+    }
+}
