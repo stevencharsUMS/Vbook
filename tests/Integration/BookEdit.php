05b53dd3c347d95a25de6ba7fc20de53de908bb7
Carl Brandon Valentine <brandonvalentine23@gmail.com>
2025-01-14 00:43:42 +0800

    Book Edit Test

---
+++ tests/Integration/BookEdit.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BookEdit extends TestCase
+{
+    /**
+     * Test account balance can be set and retrieved.
+     */
+    public function testAccountBalance()
+    {
+
+        // Assert: Check if the balance is 100
+        $this->assertEquals(100, 100);
+    }
+}
